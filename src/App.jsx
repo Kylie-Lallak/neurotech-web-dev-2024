29158216997799463b2c931f019f5cc009dc1b4e
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-08-28 20:29:46 -0700

    Added the page routes + and added example pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import Divisons from './pages/Divisons';
 
 
 function App() {
 
 
   return (
-    <>
-    <h1>Hello</h1>
-    
-    </>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/divisons' element={<Divisons />} />
+
+      </Routes>
+
+      <Footer />
+
+
+    </BrowserRouter>
   )
 }
 
