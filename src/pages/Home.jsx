819c726d8359cd08f6288efc7f8dc46b64fb624f
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-09-06 19:35:50 -0700

    home page for mobile finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import '../assets/styles/Home.css'
 
+import Arrow from '../assets/images/arrow.png'
+import HomeImage from '../assets/images/homeImage.png'
+
 export default function Home() {
   return (
-    <div>
-      Home
+    <div id='home-container' className='flex flex-col justify-center items-center'>
+      <div id='home-intro-text'>
+
+        <h1 id='home-title' className='text-5xl text-white sm:text-red-700 md:text-yellow-500 lg:text-green-600'> Neurotech <br /> @Davis</h1>
+
+        <p id='home-sub-text' className='text-lg mr-10 sm:mr-28'> Whether you're curious about BCIs or eager to innovate, you're in the right place. Let's push boundaries together!</p>
+
+        <button id='home-button'> Learn more <img id='arrow' src={Arrow} /></button>
+
+      </div>
+
+      <img className='max-w-sm mt-16 pb-6' src={HomeImage} />
+
     </div>
   )
 }
