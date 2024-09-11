2941906c3ecd7c68cf395c54895edf26c0d7dbdc
Kylie Lallak <140771878+Kylie-Lallak@users.noreply.github.com>
2024-09-11 16:33:45 -0700

    about page what we do section mobile

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import '../assets/styles/About.css';
 import HeaderImage from '../assets/images/aboutheader.png';
+import About1 from '../assets/images/aboutIcon1.png';
+import About2 from '../assets/images/aboutIcon2.png';
+import About3 from '../assets/images/aboutIcon3.png';
+import About4 from '../assets/images/aboutIcon4.png';
+import WhatImg from '../assets/images/whatimage.png';
 
 export default function About() {
     return (
+
         <div id='about-background'>
 
             <div id='about-header' className='flex flex-col items-center '>
@@ -20,6 +26,60 @@ export default function About() {
                     <img src={HeaderImage} className=' w-auto h-auto mx-auto md:min-w-[325px] md:ml-4 lg:max-w-none' />
 
                 </div>
+
+            </div>
+
+
+
+            <div id='what-sect' className='flex flex-col justify-center items-center'>
+
+                <h1 className='text-4xl font-bold mt-10 mb-5'>What We Do</h1>
+
+                <div id='what-sect-info' className='flex flex-col justify-center items-center'>
+
+                    <div className='what-card'>
+                        <img src={About1} className='what-card-img' />
+
+                        <div>
+                            <h2 className='what-card-title'>Building BCIs</h2>
+                            <p className='max-w-[250px] text-gray-700'>Design and develop BCIs linking the brain to digital systems, from wearable devices to software</p>
+                        </div>
+
+                    </div>
+
+                    <div className='what-card'>
+                        <img src={About2} className='what-card-img' />
+
+                        <div>
+                            <h2 className='what-card-title'>Startup Innovation</h2>
+                            <p className='max-w-[250px] text-gray-700'>Engage in neurotech startups, we offer a platform to develop ideas and create solutions.</p>
+                        </div>
+
+                    </div>
+
+                    <div className='what-card'>
+                        <img src={About3} className='what-card-img' />
+
+                        <div>
+                            <h2 className='what-card-title'>Publishing Knowledge</h2>
+                            <p className='max-w-[250px] text-gray-700'>Publish articles to contribute to the global neurotech feild.</p>
+                        </div>
+
+                    </div>
+
+                    <div className='what-card'>
+                        <img src={About4} className='what-card-img' />
+
+                        <div>
+                            <h2 className='what-card-title'>Educational Outreach</h2>
+                            <p className='max-w-[250px] text-gray-700'>Educate  about neurotechnology in high schools and middle schools.</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <img src={WhatImg} className='h-auto max-w-[380px]' />
 
             </div>
 
